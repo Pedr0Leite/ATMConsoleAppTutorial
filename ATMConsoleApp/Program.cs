56318c694fcf4ab0f5deafd67e6e652ba4116024
Pedr0Leite <pedromiguelgoncalvesleite@gmail.com>
2023-09-12 17:02:04 +0100

    first files

---
+++ ATMConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class cardHolder
+{
+    String cardnum;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardnum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardnum = cardnum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public String getNum() { 
+        return cardnum;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public double getBalance()
+    {
+        return balance;
+
+    }
+
+    public void setNum(String newCardNum)
+    {
+        cardnum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(String newFirstName) { 
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+}
